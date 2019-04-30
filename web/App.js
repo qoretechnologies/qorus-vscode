778a7f4f8e7e50243f89a24a3d8547957640dddf
Filip Witosz <filip.witosz@gmail.com>
2019-04-30 09:05:36 +0200

    Added typescript support to the web, added eslint & prettier configs.

--- web/App.js
+++ web/App.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Button, Navbar, NavbarGroup } from '@blueprintjs/core';
-import { ProjectConfigContainer as ProjectConfig } from './project_config/ProjectConfig';
-import { ReleasePackageContainer as ReleasePackage } from './release_package/ReleasePackage';
-import { DeleteInterfacesContainer as DeleteInterfaces } from './delete_interfaces/DeleteInterfaces';
-import { CreateInterfaceContainer as CreateInterface } from './create_interface/CreateInterface';
-import { vscode } from './common/vscode';
-import logo from '../images/qorus_logo_256.png';
-
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Button, Navbar, NavbarGroup } from "@blueprintjs/core";
+import { ProjectConfigContainer as ProjectConfig } from "./project_config/ProjectConfig";
+import { ReleasePackageContainer as ReleasePackage } from "./release_package/ReleasePackage";
+import { DeleteInterfacesContainer as DeleteInterfaces } from "./delete_interfaces/DeleteInterfaces";
+import { CreateInterfaceContainer as CreateInterface } from "./create_interface/CreateInterface";
+import { vscode } from "./common/vscode";
+import logo from "../images/qorus_logo_256.png";
 
 class App extends Component {
     constructor() {
@@ -16,15 +15,15 @@ class App extends Component {
         this.texts = {};
         this.num_text_requests = 0;
 
-        window.addEventListener('message', event => {
+        window.addEventListener("message", event => {
             switch (event.data.action) {
-                case 'return-text':
+                case "return-text":
                     this.texts[event.data.text_id] = event.data.text;
                     if (--this.num_text_requests == 0) {
                         this.forceUpdate();
                     }
                     break;
-                case 'set-active-tab':
+                case "set-active-tab":
                     this.props.setActiveTab(event.data.active_tab);
                     break;
             }
@@ -36,53 +35,77 @@ class App extends Component {
             return this.texts[text_id];
         }
         vscode.postMessage({
-            action: 'get-text',
-            text_id: text_id
+            action: "get-text",
+            text_id: text_id,
         });
         this.num_text_requests++;
-    }
+    };
 
     render() {
         const t = this.t;
         const dict_length = Object.keys(this.texts).length;
 
         const Tabs = {
-            'ProjectConfig': 'cog',
-            'ReleasePackage': 'cube',
-            'DeleteInterfaces': 'trash',
-            'CreateInterface': 'draw'
+            ProjectConfig: "cog",
+            ReleasePackage: "cube",
+            DeleteInterfaces: "trash",
+            CreateInterface: "draw",
         };
 
         return (
             <>
                 <Navbar fixedToTop={true}>
                     <NavbarGroup>
-                        <img style={{ maxWidth: 30, maxHeight: 30, marginRight: 54 }} src={logo} />
-                        {Object.keys(Tabs).map(tab_key =>
-                            <Button minimal={true} icon={Tabs[tab_key]} key={tab_key}
+                        <img
+                            style={{
+                                maxWidth: 30,
+                                maxHeight: 30,
+                                marginRight: 54,
+                            }}
+                            src={logo}
+                        />
+                        {Object.keys(Tabs).map(tab_key => (
+                            <Button
+                                minimal={true}
+                                icon={Tabs[tab_key]}
+                                key={tab_key}
                                 active={tab_key == this.props.active_tab}
                                 onClick={() => this.props.setActiveTab(tab_key)}
-                                text={t(tab_key + '-buttonText')} title={t(tab_key + '-buttonTitle')}
+                                text={t(tab_key + "-buttonText")}
+                                title={t(tab_key + "-buttonTitle")}
                             />
-                        )}
+                        ))}
                     </NavbarGroup>
                 </Navbar>
 
-                {this.props.active_tab == 'ProjectConfig' && <ProjectConfig t={this.t} _={dict_length}/>}
-                {this.props.active_tab == 'ReleasePackage' && <ReleasePackage t={this.t} _={dict_length}/>}
-                {this.props.active_tab == 'DeleteInterfaces' && <DeleteInterfaces t={this.t} _={dict_length}/>}
-                {this.props.active_tab == 'CreateInterface' && <CreateInterface t={this.t} _={dict_length}/>}
+                {this.props.active_tab == "ProjectConfig" && (
+                    <ProjectConfig t={this.t} _={dict_length} />
+                )}
+                {this.props.active_tab == "ReleasePackage" && (
+                    <ReleasePackage t={this.t} _={dict_length} />
+                )}
+                {this.props.active_tab == "DeleteInterfaces" && (
+                    <DeleteInterfaces t={this.t} _={dict_length} />
+                )}
+                {this.props.active_tab == "CreateInterface" && (
+                    <CreateInterface t={this.t} _={dict_length} />
+                )}
             </>
         );
     }
 }
 
 const mapStateToProps = state => ({
-    active_tab: state.active_tab
+    active_tab: state.active_tab,
 });
 
 const mapDispatchToProps = dispatch => ({
-    setActiveTab: tab_key => {dispatch({type: 'active_tab', active_tab: tab_key});}
+    setActiveTab: tab_key => {
+        dispatch({ type: "active_tab", active_tab: tab_key });
+    },
 });
 
-export const AppContainer = connect(mapStateToProps, mapDispatchToProps)(App);
+export const AppContainer = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(App);
--- webpack.config.js
+++ webpack.config.js
@@ -1,77 +1,80 @@
-const path = require('path');
-const root = path.resolve(__dirname, './');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const root = path.resolve(__dirname, "./");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: {
-        webview: './web/index.js',
-        login: './web/login/main.js',
-    },
-    output: {
-        path: `${root}/dist`,
-        filename: '[name].bundle.js',
-        chunkFilename: '[name].bundle.js',
-        pathinfo: false,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: { cacheDirectory: true }
-                }
-            },
-            {
-                test: /\.(s)?css$/,
-                use: [
-                    'style-loader',
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader'
-                ]
-            },
-            {
-                test: /\.(jpg|png|svg)$/,
-                loader: 'url-loader',
-            },
-            {
-                test: /\.(html|svg)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]',
-                        },
-                    },
-                ],
-            },
-        ],
-    },
-    devServer: {
-        host: 'localhost',
-        port: 9876,
-        publicPath: __dirname,
-        writeToDisk: true,
-        inline: true,
-        noInfo: false,
-        stats: { colors: true },
-    },
-    plugins: [
-        new MiniCssExtractPlugin({filename: '[name].css', chunkFilename: '[name].css'})
-    ],
-    optimization: {
-        runtimeChunk: false,
-        splitChunks: {
-            cacheGroups: {
-                vendors: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: 'vendors',
-                    enforce: true,
-                    chunks: 'all',
-                },
-            },
-        },
-    },
+  entry: {
+    webview: "./web/index.js",
+    login: "./web/login/main.js"
+  },
+  output: {
+    path: `${root}/dist`,
+    filename: "[name].bundle.js",
+    chunkFilename: "[name].bundle.js",
+    pathinfo: false
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts|tsx|js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: { cacheDirectory: true }
+        }
+      },
+      {
+        test: /\.(s)?css$/,
+        use: [
+          "style-loader",
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.(jpg|png|svg)$/,
+        loader: "url-loader"
+      },
+      {
+        test: /\.(html|svg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]"
+            }
+          }
+        ]
+      }
+    ]
+  },
+  devServer: {
+    host: "localhost",
+    port: 9876,
+    publicPath: __dirname,
+    writeToDisk: true,
+    inline: true,
+    noInfo: false,
+    stats: { colors: true }
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[name].css"
+    })
+  ],
+  optimization: {
+    runtimeChunk: false,
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          enforce: true,
+          chunks: "all"
+        }
+      }
+    }
+  }
 };
