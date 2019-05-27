1b34276dda832df8d277a30f84d4c6babb24d6aa
Martin Zemek <martin.zemek@qoretechnologies.com>
2019-05-27 17:38:04 +0200

    refs #71 - code object info cache from the result of qop.q (not finished)

--- src/QorusProjectCodeInfo.ts
+++ src/QorusProjectCodeInfo.ts
@@ -1,10 +1,14 @@
+import * as child_process from 'child_process';
 import { t } from 'ttag';
 import * as msg from './qorus_message';
 
 
+const object_parser_command = 'qop.q';
+
 export class QorusProjectCodeInfo {
     private project_folder: string;
     private pending: boolean = false;
+//    private code_info: any = {};
 
     constructor(project_folder: string) {
         this.project_folder = project_folder;
@@ -12,23 +16,36 @@ export class QorusProjectCodeInfo {
     }
 
     update() {
-        setTimeout(this.doAsyncUpdate, 0);
+        setTimeout(() => this.doAsyncUpdate(), 0);
 
         msg.log(t`CodeInfoUpdateStarted ${this.project_folder}`);
         this.pending = true;
     }
 
     private doAsyncUpdate() {
-        let s = 0, p = -1;
-        for (let i = 1; i < 3000000000; i++, p = -p) {
-            s += p * i;
-        }
+        let command_parts: string[] = this.allProjectSourceFiles();
+        command_parts.unshift(object_parser_command);
+        const command: string = command_parts.join(' ');
+
+        const result: Buffer = child_process.execSync(command);
+        const objects: any[] = JSON.parse(result.toString());
+        msg.log('objects: ' + JSON.stringify(objects, null, 4));
+
+
+
+
+
+
+
         this.pending = false;
         msg.log(t`CodeInfoUpdateFinished ${this.project_folder}`);
-        return s;
     }
 
     get update_pending(): boolean {
         return this.pending;
+    }
+
+    private allProjectSourceFiles(): string[] {
+        return ['/home/martin/src/drei/erp-quk/src/010-RECODE_ADJUSTMENT-ANOVO-IN/uk-010-recode_adjustment-anovo-in.qsd'];
     }
 }
