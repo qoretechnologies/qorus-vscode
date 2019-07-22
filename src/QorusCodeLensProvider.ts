4f984009136e1a17cabae0011b6a6d765a3d8e11
Martin Zemek <martin.zemek@qoretechnologies.com>
2019-07-22 09:16:52 +0200

    refs #71 - changed document type in getDocumentSymbols()

--- src/QorusCodeLensProvider.ts
+++ src/QorusCodeLensProvider.ts
@@ -5,11 +5,26 @@ import { methodName } from './qorus_utils';
 import { t } from 'ttag';
 import * as msg from './qorus_message';
 
+export interface QoreTextDocument {
+    uri: string,
+    text: string,
+    languageId: string,
+    version: number
+};
+
 export class QorusCodeLensProvider implements vscode.CodeLensProvider {
 
     public provideCodeLenses(document: vscode.TextDocument): Promise<vscode.CodeLens[]> {
         const yaml_info = projects.getProject().code_info.yaml_info;
-        return qore_vscode.exports.getDocumentSymbols(document).then(symbols => {
+
+        const doc: QoreTextDocument = {
+            uri: 'file:' + document.uri.path,
+            text: document.getText(),
+            languageId: document.languageId,
+            version: document.version
+        };
+
+        return qore_vscode.exports.getDocumentSymbols(doc).then(symbols => {
             let lenses: vscode.CodeLens[] = [];
 
             symbols.forEach(symbol => {
