03eacfa5fb7d7be370bbe5c4558cfa66df5272d8
Martin Zemek <martin.zemek@qoretechnologies.com>
2019-08-27 17:12:24 +0200

    refs #71 - creating class fixed

--- src/qorus_creator/ClassCreator.ts
+++ src/qorus_creator/ClassCreator.ts
@@ -16,7 +16,7 @@ class ClassCreator extends InterfaceCreator {
         let message: string;
         switch (edit_type) {
             case 'create':
-                contents = this.fillTemplate(class_template, { class_name: data.class_name });
+                contents = this.fillTemplate(class_template, { class_name: data.name });
                 message = t`2FilesCreatedInDir ${this.file_name} ${this.yaml_file_name} ${this.target_dir}`;
                 break;
             default:
