53aa5521ef7fb5b628934f63a0cf3c5d3552e734
Martin Zemek <martin.zemek@qoretechnologies.com>
2020-02-18 16:16:54 +0100

    refs #364 fixed: step-type is computed to early, before the inheritance information is known

--- src/QorusProjectCodeInfo.ts
+++ src/QorusProjectCodeInfo.ts
@@ -591,6 +591,15 @@ export class QorusProjectCodeInfo {
             data['steps-info'] = this.stepData(data.steps);
         }
 
+        if (data.type === 'step' && data['base-class-name']) {
+            const step_type = (data.lang === 'java')
+                ? this.javaStepType(data['base-class-name'])
+                : this.stepType(data['base-class-name']);
+            if (step_type) {
+                data['step-type'] = step_type;
+            }
+        }
+
         if (!data.target_file && data.yaml_file) {
             data.target_file = path.basename(data.yaml_file);
         }
@@ -1183,15 +1192,6 @@ export class QorusProjectCodeInfo {
 
         if (base_class_name) {
             this.yaml_data[file]['base-class-name'] = base_class_name;
-
-            if (yaml_data.type === 'step') {
-                const step_type = (yaml_data.lang === 'java')
-                    ? this.javaStepType(base_class_name)
-                    : this.stepType(base_class_name);
-                if (step_type) {
-                    this.yaml_data[file]['step-type'] = step_type;
-                }
-            }
         }
 
         if (class_name && base_class_name && ['class', 'step'].includes(yaml_data.type)) {
