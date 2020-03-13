747ce931cd8e669044460cd431bf7a4789c505f8
Martin Zemek <martin.zemek@qoretechnologies.com>
2020-03-13 09:50:54 +0100

    params for getFields() call written in a different way

--- src/QorusWebview.ts
+++ src/QorusWebview.ts
@@ -193,9 +193,8 @@ class QorusWebview {
                                 action: 'creator-return-fields',
                                 iface_kind: message.iface_kind,
                                 fields: creator.getSortedFields({
+                                    ... message,
                                     interface_info,
-                                    iface_kind: message.iface_kind,
-                                    is_editing: message.is_editing || false,
                                     default_target_dir: initial_data.uri && initial_data.uri.fsPath,
                                 }),
                             });
