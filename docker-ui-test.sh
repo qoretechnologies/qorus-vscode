c735d14ede99995c42cf5fe048ea5d39c4a47b38
Ondřej Musil <ondrej.musil@qoretechnologies.com>
2020-05-07 12:14:36 +0200

    Test script fixes

--- docker-ui-test.sh
+++ docker-ui-test.sh
@@ -7,6 +7,16 @@ export rootdir=`pwd`
 export PATH=${rootdir}/node_modules/.bin:${PATH}
 export DISPLAY=:0
 export XAUTHORITY=${HOME}/.Xauthority
+export PROJECT_FOLDER=${rootdir}/ui-test/test_project
+
+find_active_monitor() {
+    x_output=`xrandr -q | grep "connected primary" | xargs | cut -d' ' -f1`
+    [ "$x_output" == "" ] && return 1
+    return 0
+}
+
+# cleanup vscode settings
+rm -rf ${HOME}/.vscode
 
 # install
 cd ${rootdir}
@@ -32,11 +42,18 @@ npm run compile:test
 cd ${rootdir}
 ui-test/node_modules/.bin/extest setup-tests
 
-# set vscode resolution
+# set resolution
+if find_active_monitor; then
+    xrandr --output ${x_output} --mode 1920x1200
+else
+    echo "Cannot find active monitor -> cannot set screen resolution" >&2
+    exit 1
+fi
 sed -i 's/width:1024,height:768/width:1920,height:1080/' test-resources/VSCode-linux-x64/resources/app/out/vs/code/electron-main/main.js
 
 # install qore extension
-test-resources/VSCode-linux-x64/bin/code --user-data-dir test-resources/settings --install-extension qoretechnologies.qore-vscode
+#test-resources/VSCode-linux-x64/bin/code --user-data-dir test-resources/settings --install-extension qoretechnologies.qore-vscode
+test-resources/VSCode-linux-x64/bin/code --install-extension qoretechnologies.qore-vscode
 
 # run tests
 ui-test/node_modules/.bin/extest run-tests ui-test/out/*.js
