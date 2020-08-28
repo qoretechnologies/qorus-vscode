a530faa6a7f4842e631e6aba6038f01080f2dcc4
Martin Zemek <martin.zemek@qoretechnologies.com>
2020-08-28 14:30:55 +0200

    use setFileInfo() in code lens providers properly

--- src/QorusJavaCodeLensProvider.ts
+++ src/QorusJavaCodeLensProvider.ts
@@ -4,39 +4,52 @@ import { QorusProjectEditInfo } from './QorusProjectEditInfo';
 import { QorusCodeLensProviderBase } from './QorusCodeLensProviderBase';
 import { QorusJavaParser }  from './QorusJavaParser';
 import { javaLoc2Range } from './QoreTextDocument';
+import * as msg from './qorus_message';
 
 export class QorusJavaCodeLensProvider extends QorusCodeLensProviderBase {
-    protected async provideLanguageSpecificImpl(_document: TextDocument, file_path: string, iface_kind: string, data: any): Promise<CodeLens[]> {
+    protected async provideLanguageSpecificImpl(
+        _document: TextDocument,
+        file_path: string,
+        iface_kind: string,
+        data: any): Promise<CodeLens[]>
+    {
+        return this.code_info.edit_info.setFileInfo(file_path, data).then(
+            () => {
+                let lenses: CodeLens[] = [];
+                data = this.code_info.fixData(data);
 
-        await this.code_info.edit_info.setFileInfo(file_path, data);
-        let lenses: CodeLens[] = [];
-        data = this.code_info.fixData(data);
+                const parsed_data: any = QorusJavaParser.parseFileNoExcept(file_path);
+                parsed_data.classes.forEach(parsed_class => {
+                    if (!QorusProjectEditInfo.isJavaSymbolExpectedClass(parsed_class, data['class-name'])) {
+                        return;
+                    }
 
-        const parsed_data: any = QorusJavaParser.parseFileNoExcept(file_path);
-        parsed_data.classes.forEach(parsed_class => {
-            if (!QorusProjectEditInfo.isJavaSymbolExpectedClass(parsed_class, data['class-name'])) {
-                return;
+                    this.addClassLenses(iface_kind, lenses, javaLoc2Range(parsed_class.name.loc), data);
+
+                    if (!['service', 'mapper-code'].includes(iface_kind)) {
+                        return;
+                    }
+
+                    for (const method of parsed_class.body.methods || []) {
+                        if (!QorusProjectEditInfo.isJavaDeclPublicMethod(method)) {
+                            continue;
+                        }
+
+                        this.addMethodLenses(
+                            iface_kind, lenses, javaLoc2Range(method.name.loc), data,
+                            method.name.identifier, parsed_class.name.identifier
+                        );
+                    }
+                });
+
+                this.previous_lenses = lenses;
+                return lenses;
+            },
+
+            error => {
+                msg.error(error);
+                return this.previous_lenses;
             }
-
-            this.addClassLenses(iface_kind, lenses, javaLoc2Range(parsed_class.name.loc), data);
-
-            if (!['service', 'mapper-code'].includes(iface_kind)) {
-                return;
-            }
-
-            for (const method of parsed_class.body.methods || []) {
-                if (!QorusProjectEditInfo.isJavaDeclPublicMethod(method)) {
-                    continue;
-                }
-
-                this.addMethodLenses(
-                    iface_kind, lenses, javaLoc2Range(method.name.loc), data,
-                    method.name.identifier, parsed_class.name.identifier
-                );
-            }
-
-            this.previous_lenses = lenses;
-        });
-        return lenses;
+        );
     }
 }
--- src/QorusQoreCodeLensProvider.ts
+++ src/QorusQoreCodeLensProvider.ts
@@ -5,47 +5,63 @@ import { QorusProjectEditInfo } from './QorusProjectEditInfo';
 import { QoreTextDocument, qoreLoc2Range } from './QoreTextDocument';
 import { makeFileUri } from './qorus_utils';
 import { qore_vscode } from './qore_vscode';
+import * as msg from './qorus_message';
 
 export class QorusQoreCodeLensProvider extends QorusCodeLensProviderBase {
-    protected provideLanguageSpecificImpl(document: TextDocument, file_path: string, iface_kind: string, data: any): Promise<CodeLens[]> {
-        const doc: QoreTextDocument = {
-            uri: makeFileUri(file_path),
-            text: document.getText(),
-            languageId: document.languageId,
-            version: document.version
-        };
+    protected provideLanguageSpecificImpl(
+        document: TextDocument,
+        file_path: string,
+        iface_kind: string,
+        data: any): Promise<CodeLens[]>
+    {
+        return this.code_info.edit_info.setFileInfo(file_path, data).then(
+            () => {
+                const doc: QoreTextDocument = {
+                    uri: makeFileUri(file_path),
+                    text: document.getText(),
+                    languageId: document.languageId,
+                    version: document.version
+                };
 
-        return qore_vscode.exports.getDocumentSymbols(doc, 'node_info').then(symbols => {
-            if (!symbols.length) {
-                return this.previous_lenses;
-            }
-            let lenses: CodeLens[] = [];
-            data = this.code_info.fixData(data);
-
-            symbols.forEach(symbol => {
-                if (!QorusProjectEditInfo.isQoreSymbolExpectedClass(symbol, data['class-name'])) {
-                    return;
-                }
-
-                this.addClassLenses(iface_kind, lenses, qoreLoc2Range(symbol.name.loc), data);
-
-                if (!['service', 'mapper-code'].includes(iface_kind)) {
-                    return;
-                }
-
-                for (const decl of symbol.declarations || []) {
-                    if (!QorusProjectEditInfo.isQoreDeclPublicMethod(decl)) {
-                        continue;
+                return qore_vscode.exports.getDocumentSymbols(doc, 'node_info').then(symbols => {
+                    if (!symbols.length) {
+                        return this.previous_lenses;
                     }
 
-                    const method_name = decl.name.name;
-                    const name_range = qoreLoc2Range(decl.name.loc);
-                    this.addMethodLenses(iface_kind, lenses, name_range, data, method_name);
-                }
-            });
+                    let lenses: CodeLens[] = [];
+                    data = this.code_info.fixData(data);
 
-            this.previous_lenses = lenses;
-            return lenses;
-        });
+                    symbols.forEach(symbol => {
+                        if (!QorusProjectEditInfo.isQoreSymbolExpectedClass(symbol, data['class-name'])) {
+                            return;
+                        }
+    
+                        this.addClassLenses(iface_kind, lenses, qoreLoc2Range(symbol.name.loc), data);
+    
+                        if (!['service', 'mapper-code'].includes(iface_kind)) {
+                            return;
+                        }
+    
+                        for (const decl of symbol.declarations || []) {
+                            if (!QorusProjectEditInfo.isQoreDeclPublicMethod(decl)) {
+                                continue;
+                            }
+    
+                            const method_name = decl.name.name;
+                            const name_range = qoreLoc2Range(decl.name.loc);
+                            this.addMethodLenses(iface_kind, lenses, name_range, data, method_name);
+                        }
+                    });
+    
+                    this.previous_lenses = lenses;
+                    return lenses;
+                });
+            },
+
+            error => {
+                msg.error(error);
+                return this.previous_lenses;
+            }
+        );
     }
 }
