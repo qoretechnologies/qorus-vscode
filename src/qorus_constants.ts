bc87bce6e3d2997f954ac3edba9cbdf9881ac4f2
Martin Zemek <martin.zemek@qoretechnologies.com>
2020-09-24 15:44:35 +0200

    refs #109 added connection-related menu items and tree items

--- src/qorus_constants.ts
+++ src/qorus_constants.ts
@@ -1,16 +1,20 @@
 export const types_with_version = ['step', 'mapper'];
-export const types_without_version = ['service', 'job', 'workflow', 'config-item-values', 'config-items',
-                               'class', 'constant', 'function', 'connection', 'event', 'group',
-                               'queue', 'value-map', 'mapper-code', 'type', 'fsm', 'pipeline'];
+export const types_without_version = [
+    'service', 'job', 'workflow', 'config-item-values', 'config-items',
+    'class', 'constant', 'function', 'connection', 'event', 'group',
+    'queue', 'value-map', 'mapper-code', 'type', 'fsm', 'pipeline'
+];
 export const types = [...types_with_version, ...types_without_version];
 
 export const root_service = 'QorusService';
 export const root_job = 'QorusJob';
 export const root_workflow = 'QorusWorkflow';
 export const root_processor = 'AbstractDataProcessor';
-export const root_steps = ['QorusAsyncStep', 'QorusEventStep', 'QorusNormalStep', 'QorusSubworkflowStep',
-                           'QorusAsyncArrayStep', 'QorusEventArrayStep',
-                           'QorusNormalArrayStep', 'QorusSubworkflowArrayStep'];
+export const root_steps = [
+    'QorusAsyncStep', 'QorusEventStep', 'QorusNormalStep', 'QorusSubworkflowStep',
+    'QorusAsyncArrayStep', 'QorusEventArrayStep',
+    'QorusNormalArrayStep', 'QorusSubworkflowArrayStep'
+];
 export const all_root_classes = [...root_steps, root_service, root_job, root_workflow, root_processor];
 
 export const default_version = '1.0';
