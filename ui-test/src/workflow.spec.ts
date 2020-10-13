d592a34c7a7a3298076ee59361341f1a0ab79bb2
Martin Zemek <martin.zemek@qoretechnologies.com>
2020-10-13 16:32:24 +0200

    added workflow tests

---
+++ ui-test/src/workflow.spec.ts
@@ -0,0 +1,28 @@
+import { EditorView, VSBrowser, WebDriver, WebView, Workbench } from 'vscode-extension-tester';
+
+import { setupWebview } from './common/utils';
+import { createWorkflow, openCreateWorkflow } from './tests/workflow';
+
+describe('Workflow tests', function () {
+    this.timeout(1800000);
+    let driver: WebDriver;
+    let workbench: Workbench;
+    let editorView: EditorView;
+    let webview: WebView;
+    const project_folder: string = process.env.PROJECT_FOLDER || '/builds/mirror/qorus-vscode/ui-test/test_project';
+
+    before(async () => {
+        driver = VSBrowser.instance.driver;
+        ({ workbench, editorView, webview } = await setupWebview());
+    });
+
+    // create workflow tests
+    it('Opens workflow create page', () => openCreateWorkflow(webview));
+    it('Can create workflow', () => createWorkflow(webview, editorView, project_folder));
+
+    // Reset the workbench
+    this.afterAll(async () => {
+        webview.switchBack();
+        editorView.closeAllEditors();
+    });
+});
