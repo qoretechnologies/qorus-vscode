51e9633ce1b51db1a372acc0169712a46aa1157e
Martin Zemek <martin.zemek@qoretechnologies.com>
2020-10-15 10:31:04 +0200

    mapper test removed, only one fsm step left

--- ui-test/src/fsm.spec.ts
+++ ui-test/src/fsm.spec.ts
@@ -17,8 +17,8 @@ describe('FSM tests', function () {
         ({ workbench, editorView, webview } = await setupWebview());
     });
 
-    it('Login', async () => login(webview));
-    it('Opens FSM create page', () => openFSMPage(webview));
+    it('Login', () => login(webview));
+//    it('Opens FSM create page', () => openFSMPage(webview));
 
     // Reset the workbench
     this.afterAll(async () => {
--- ui-test/src/tests/login.ts
+++ ui-test/src/tests/login.ts
@@ -3,7 +3,7 @@ import { By, WebView } from 'vscode-extension-tester';
 import { sleep, clickElement } from '../common/utils';
 
 export const login = async (webview: WebView) => {
-    await sleep(1000);
+    await sleep(3000);
     const environmentPanels = await webview.findWebElements(By.className('env-panel'));
     expect(environmentPanels).to.have.length(4);
     await clickElement(webview, 'set-active-instance', 7);
