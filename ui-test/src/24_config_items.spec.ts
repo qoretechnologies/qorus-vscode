914d9522de3ce7d7150b77dda7fed6279df317b6
Martin Zemek <martin.zemek@qoretechnologies.com>
2020-10-15 16:52:54 +0200

    missing 'async'

--- ui-test/src/24_config_items.spec.ts
+++ ui-test/src/24_config_items.spec.ts
@@ -19,7 +19,7 @@ describe('Config Items Tests', function () {
     it('Create class', () => createClass(webview, editorView));
     it('Create service', () => createService(webview, editorView));
     it('Check files', () => checkFiles(project_folder));
-    it('Edit interface', () => {
+    it('Edit interface', async () => {
         webview = await editInterface(workbench, editorView, project_folder);
     });
 
