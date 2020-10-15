6a394ad41d9850a4c36698c94790a81aa40f83ae
Martin Zemek <martin.zemek@qoretechnologies.com>
2020-10-15 10:06:35 +0200

    added fms and mapper tests

---
+++ ui-test/src/fsm.spec.ts
@@ -0,0 +1,28 @@
+import { EditorView, VSBrowser, WebDriver, WebView, Workbench } from 'vscode-extension-tester';
+
+import { setupWebview } from './common/utils';
+import { login } from './tests/login';
+import { openFSMPage } from './tests/fsm';
+
+describe('FSM tests', function () {
+    this.timeout(1800000);
+    let driver: WebDriver;
+    let workbench: Workbench;
+    let editorView: EditorView;
+    let webview: WebView;
+    const project_folder: string = process.env.PROJECT_FOLDER || '/builds/mirror/qorus-vscode/ui-test/test_project';
+
+    before(async () => {
+        driver = VSBrowser.instance.driver;
+        ({ workbench, editorView, webview } = await setupWebview());
+    });
+
+    it('Login', async () => login(webview));
+    it('Opens FSM create page', () => openFSMPage(webview));
+
+    // Reset the workbench
+    this.afterAll(async () => {
+        webview.switchBack();
+        editorView.closeAllEditors();
+    });
+});
---
+++ ui-test/src/mapper.spec.ts
@@ -0,0 +1,31 @@
+import { EditorView, VSBrowser, WebDriver, WebView, Workbench } from 'vscode-extension-tester';
+
+import { setupWebview } from './common/utils';
+import { login } from './tests/login';
+import { createMapper, checkFile, editMapper } from './tests/mapper';
+
+describe('Mapper Tests', function () {
+    this.timeout(1800000);
+    let driver: WebDriver;
+    let workbench: Workbench;
+    let editorView: EditorView;
+    let webview: WebView;
+    const project_folder: string = process.env.PROJECT_FOLDER || '/builds/mirror/qorus-vscode/ui-test/test_project';
+
+    before(async () => {
+        driver = VSBrowser.instance.driver;
+        ({ workbench, editorView, webview } = await setupWebview());
+    });
+
+//    login already is in the fsm test ... should be put in an extra test at the beginning
+//    it('Login', () => login(webview));
+    it('Create mapper', () => createMapper(webview));
+    it('Check file', () => checkFile(project_folder, 0));
+//    it('Edit mapper', () => editMapper(webview, workbench, project_folder));
+//    it('Check changed file', () => checkFile(project_folder, 1));
+
+    this.afterAll(async () => {
+        webview.switchBack();
+        editorView.closeAllEditors();
+    });
+});
