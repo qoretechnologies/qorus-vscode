c36f32d605c11b65f5fd2f8dff75206c1dd520bb
Martin Zemek <martin.zemek@qoretechnologies.com>
2020-11-11 18:14:08 +0100

    refs #168 field definition for value maps

--- src/interface_creator/other_constants.ts
+++ src/interface_creator/other_constants.ts
@@ -20,3 +20,49 @@ export const queueFields = ({ default_target_dir }) => [
     field.name,
     field.desc,
 ];
+
+export const valueMapFields = ({ default_target_dir }) => [
+    field.targetDir(default_target_dir),
+    field.targetFile,
+    field.name,
+    {
+        ... field.desc,
+        mandatory: false
+    },
+    field.author,
+    {
+        name: 'exception',
+        mandatory: false,
+        type: 'boolean',
+        default_value: false,
+    },
+    field.groups,
+    {
+        name: 'valuetype',
+        mandatory: false,
+        type: 'enum',
+        items: [
+            { value: 'int' },
+            { value: 'string' },
+            { value: 'float' },
+            { value: 'raw' },
+            { value: 'date' },
+        ],
+        notify_on_add: true,
+        notify_on_remove: true,
+        on_change: 'valuetype-changed',
+    },
+    {
+        name: 'dateformat',
+        mandatory: false,
+        default_value: 'DD.MM.YYYY',
+        notify_on_add: true,
+        notify_on_remove: true,
+    },
+    {
+        name: 'value-maps',
+        mandatory: false,
+        type: 'array-of-pairs',
+        fields: ['key', 'value'],
+    },
+];
