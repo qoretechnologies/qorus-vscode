da84e5efd578bac36b9cbcccd78bd3f6295240a2
Filip Witosz <filip.witosz@gmail.com>
2020-11-19 17:45:00 +0100

    Test of sequential test runs.

--- docker-ui-test.sh
+++ docker-ui-test.sh
@@ -7,30 +7,11 @@ export rootdir=`pwd`
 export PATH=${rootdir}/node_modules/.bin:${PATH}
 export DISPLAY=:0
 export XAUTHORITY=${HOME}/.Xauthority
-export PROJECT_FOLDER=${rootdir}/ui-test/test_project
-export TEST_GOLD_FILES=${rootdir}/ui-test/gold_files
 
 find_active_monitor() {
     x_output=`xrandr -q | grep "connected primary" | xargs | cut -d' ' -f1`
     [ "$x_output" == "" ] && return 1
     return 0
-}
-
-video_recording() {
-    ps aux | grep "ffmpeg.*$1\.mp4" | grep -v grep  > /dev/null 2>&1
-}
-
-recording_pid() {
-    ps aux | grep "ffmpeg.*$1\.mp4" | grep -v grep | grep -v tmux | xargs | cut -d' ' -f2
-}
-
-signal_recording() {
-    video_pid=`recording_pid`
-    if [ "$video_pid" != "" ]; then
-        echo "Found PID of video recording: $video_pid"
-        echo "Sending $1 signal to recording..."
-        kill -s $1 $video_pid
-    fi
 }
 
 # cleanup vscode settings
@@ -54,11 +35,6 @@ sed -i 's/const args = \[/const args = \["--disable-gpu", /' browser.js
 # setup test environment
 cd ${rootdir}
 mkdir -p test-resources/settings
-rm -rf ui-test/out && npm run compile:test
-
-# extester setup
-cd ${rootdir}
-ui-test/node_modules/.bin/extest setup-tests
 
 # set resolution
 if find_active_monitor; then
@@ -73,36 +49,9 @@ sed -i 's/width:1024,height:768/width:1920,height:1200/' test-resources/VSCode-l
 #test-resources/VSCode-linux-x64/bin/code --user-data-dir test-resources/settings --install-extension qoretechnologies.qore-vscode
 test-resources/VSCode-linux-x64/bin/code --install-extension qoretechnologies.qore-vscode
 
-# record video
-session_name=test-`head -c 192 /dev/urandom | tr -dc A-Za-z0-9 | head -c 24`
-tmux new-session -d -s "${session_name}" "ffmpeg -f x11grab -video_size 1680x1050 -i :0 -codec:v libx264 -r 15 /tmp/qorus-vscode-uitest.mp4"
-
 # run tests
 set +e
-ui-test/node_modules/.bin/extest run-tests 'ui-test/out/*.spec.js'
+./tests.sh
 rc="$?"
-
-# send command to turn off video recording cleanly
-tmux send-keys -t "${session_name}" q
-
-# wait until video recording finishes
-set +x
-echo "Waiting for the video recording to finish..."
-i=0
-while video_recording; do
-    if [ "$i" == "60" ]; then
-        echo "5 minutes timeout for video recording to finish"
-        signal_recording INT
-    elif [ "$i" == "72" ]; then
-        echo "6 minutes timeout for video recording to finish"
-        signal_recording TERM
-    elif [ "$i" == "84" ]; then
-        echo "7 minutes timeout for video recording to finish"
-        signal_recording KILL
-    fi
-
-    sleep 5
-    i=$((i+1))
-done
 
 exit $rc
---
+++ tests.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set +e
+
+npm run ui-setup-tests
+
+rm -rf ui-test/out
+
+npm run compile:test
+
+files=./ui-test/out/*.spec.js
+
+for item in ${files[@]}
+do
+  item=$(basename -- "$item")
+  item="`echo $item|cut -f1 -d.`"
+  if [ $item != '_setup' ]; then
+    TEST_NAME=$item npm run ui-run-tests-gitlab
+    if [ $? = 1 ]; then
+      printf "${item} test failed!"
+      rm -rf ui-test/out
+      exit 1
+    fi
+  fi
+done
+
+set -e
+
+rm -rf ui-test/out
+
+exit 0
