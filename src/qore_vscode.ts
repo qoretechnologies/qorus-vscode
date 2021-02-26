fb6a3732f9c8a37c335f8f0bb3d2240996cbefe1
Martin Zemek <martin.zemek@qoretechnologies.com>
2021-02-26 12:54:57 +0100

    implemented isLangClientAvailable()

--- src/qore_vscode.ts
+++ src/qore_vscode.ts
@@ -1,2 +1,12 @@
 import { extensions } from 'vscode';
 export const qore_vscode = extensions.getExtension('qoretechnologies.qore-vscode');
+
+let is_availability_known = false;
+let is_lang_client_available = false;
+export async function isLangClientAvailable(): Promise<boolean> {
+    if (!is_availability_known) {
+        is_lang_client_available = await qore_vscode.exports.isLangClientAvailable();
+        is_availability_known = true;
+    }
+    return Promise.resolve(is_lang_client_available);
+}
