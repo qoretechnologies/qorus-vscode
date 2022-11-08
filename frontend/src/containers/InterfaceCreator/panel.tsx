92b22b8766209897f48422f44ae9ca185d9e9c3c
Foxhoundn <filip.witosz@gmail.com>
2022-11-08 10:46:36 +0000

    Checking values with `canBeNull` properly

--- frontend/src/containers/InterfaceCreator/panel.tsx
+++ frontend/src/containers/InterfaceCreator/panel.tsx
@@ -602,6 +602,7 @@ const InterfaceCreatorPanel: FunctionComponent<IInterfaceCreatorPanel> = ({
     explicit?: boolean,
     metadata?: any
   ) => void = (fieldName, value, forcedType, canBeNull, explicit, metadata) => {
+    console.log('handleFieldChange', fieldName, value, forcedType, canBeNull, explicit, metadata);
     setSelectedFields(
       type,
       (currentFields: IField[]): IField[] => {
@@ -654,7 +655,7 @@ const InterfaceCreatorPanel: FunctionComponent<IInterfaceCreatorPanel> = ({
               // If this is a single field
               if (finalFieldType === 'auto') {
                 // Validate single field
-                isValid = validateField(fieldType, value, currentField);
+                isValid = validateField(fieldType, value, currentField, canBeNull);
               } else {
                 // Check if the type is in allowed types
                 if (allowedTypes.includes(fieldType)) {
@@ -672,6 +673,7 @@ const InterfaceCreatorPanel: FunctionComponent<IInterfaceCreatorPanel> = ({
             }
             // On change events
             maybeSendOnChangeEvent(currentField, value, type, interfaceId, isEditing);
+            console.log('IS FIELD VALID LMFAO', isValid);
             // Add the value
             return [
               ...newFields,
@@ -680,6 +682,7 @@ const InterfaceCreatorPanel: FunctionComponent<IInterfaceCreatorPanel> = ({
                 value,
                 hasValueSet: !explicit,
                 isValid,
+                canBeNull,
               },
             ];
           }
