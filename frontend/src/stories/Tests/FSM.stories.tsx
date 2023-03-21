9fdd9d1fd0470cf642db5ecf0cb6e3a40f01544e
Foxhoundn <filip.witosz@gmail.com>
2023-03-21 13:19:37 +0000

    Testing tests 3

--- frontend/src/stories/Tests/FSM.stories.tsx
+++ frontend/src/stories/Tests/FSM.stories.tsx
@@ -230,22 +230,22 @@ export const NewMessageState: StoryFSM = {
       await fireEvent.click(canvas.getByText('raw'));
     });
 
-    // // Add the message data
-    // await waitFor(() => canvas.findByText(/MessageData/g), { timeout: 5000 });
-    // await await waitFor(() =>
-    //   expect(document.querySelector('.state-submit-button')).toBeDisabled()
-    // );
+    // Add the message data
+    await waitFor(() => canvas.findByText(/MessageData/g), { timeout: 5000 });
+    await await waitFor(() =>
+      expect(document.querySelector('.state-submit-button')).toBeDisabled()
+    );
 
-    // await waitFor(
-    //   async () => {
-    //     await expect(document.querySelector('.provider-message-data textarea')).toBeInTheDocument();
-    //     await userEvent.type(
-    //       document.querySelector('.provider-message-data textarea'),
-    //       'Hello World'
-    //     );
-    //   },
-    //   { timeout: 5000 }
-    // );
+    await waitFor(
+      async () => {
+        await expect(document.querySelector('.provider-message-data textarea')).toBeInTheDocument();
+        await userEvent.type(
+          document.querySelector('.provider-message-data textarea'),
+          'Hello World'
+        );
+      },
+      { timeout: 5000 }
+    );
 
     // // Submit the state
     // await waitFor(_testsSubmitFSMState, { timeout: 5000 });
